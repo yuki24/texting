561a0972cde93c4864751c784e1bbcba61270ad6
Yuki Nishijima <yk.nishijima@gmail.com>
2017-09-05 20:39:42 -0400

    Sometimes the from phone number is optional

--- lib/texting/text_message.rb
+++ lib/texting/text_message.rb
@@ -2,7 +2,7 @@ module Texting
   class TextMessage
     attr_accessor :from, :to, :body, :options
 
-    def initialize(from: , to: , body: , **options)
+    def initialize(to: , body: , from: nil, **options)
       @from, @to, @body, @options = from, to, body, options
     end
 
